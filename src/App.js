ff64690c227b234ca205ad2d4a0d52471ba367a6
Nishchay Verma <nishchayverma@Nishchays-MacBook-Air.local>
2022-06-04 04:52:31 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React, { useState, useEffect, useRef } from "react"
-import useGameLogic from "./useGameLogic"
+
 import "./style.css"
 
 function App() {
-const [textBoxRef, handleChange,text,timeRemaining,startGame,isTimeRunning,wordCount, endGame] = useGameLogic()    
     return (
         <div className="App">
             <h1>How many words can you type in 30 seconds?</h1>
             <textarea
                 data-id='textarea'
-                ref={textBoxRef}
-                onChange={handleChange}
-                value={text}
-                disabled={!isTimeRunning}
+               
             />
-            <h4 data-id="time-remaining">Time remaining: {timeRemaining}</h4>
+            <h4 data-id="time-remaining">Time remaining: show the time remaining here </h4>
             <button 
                 data-id="button"
-                onClick={startGame}
-                disabled={isTimeRunning}
+              
             >  
                 Start
             </button>
-            <h1 data-id="word-count">Word count: {wordCount}</h1>
+            <h1 data-id="word-count">Word count: Show word count here on change </h1>
             <button 
                 data-id="end-button"
-                onClick={endGame}
-                disabled={!isTimeRunning}
+               
             >  
                 End Game
             </button>
